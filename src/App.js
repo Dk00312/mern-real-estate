92907a982af44b6453a29c3ce011a72d1d40411e
Dk00312 <devkotecha2022@gmail.com>
2024-05-21 10:38:46 +0530

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home/> } />
+        <Route path="/profile" element={<Profile/> } />
+        <Route path="/about" element={ <About/>} />
+        <Route path="/sign-in" element={<SignIn/> } />
+        <Route path="/sign-out" element={<SignOut/> } />
+      </Routes>
     </div>
   );
 }
